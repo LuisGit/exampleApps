caf85e26366dcb466a67ee74a6fc72b74448b90f
Luis Alvarado Solano <luis.alvarado@gorillalogic.com>
2020-12-07 20:53:28 -0600

    updating api calls adn results

--- appsProject/app/api/yelp.js
+++ appsProject/app/api/yelp.js
@@ -1,10 +1,47 @@
 import axios from 'axios';
 import constants from '../constants';
 
-export default axios.create({
-  baseURL: constants.API_URL,
-  headers: {
-    Authorization:
-      'Bearer sAvc2yZ7l_6s7en_vOtxgIkD56sDmNxBKv3O4DTYdXAkhuFdWT82QrXTFz6AKEMmiIhO80s4gqT9yEmGmxix526QBuViveUxo-mn6xQ7CA4LmIFS300r9xQ4RNfKX3Yx',
-  },
-});
+const getHeaders = () => {
+  //const token = await getToken();
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  //   if (token) { //Token should come from an success login or an env
+  headers.Authorization =
+    'Bearer sAvc2yZ7l_6s7en_vOtxgIkD56sDmNxBKv3O4DTYdXAkhuFdWT82QrXTFz6AKEMmiIhO80s4gqT9yEmGmxix526QBuViveUxo-mn6xQ7CA4LmIFS300r9xQ4RNfKX3Yx';
+  //   }
+  return headers;
+};
+
+export const doPost = async (destination, data) => {
+  const headers = getHeaders();
+  try {
+    const response = await axios.post(
+      `${constants.API_URL}${destination}`,
+      data,
+      headers,
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const doGet = async (destination, params) => {
+  const headers = getHeaders();
+  let config = {
+    headers,
+    params,
+  };
+
+  try {
+    const response = await axios.get(
+      `${constants.API_URL}${destination}`,
+      config,
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- appsProject/app/hooks/useResults.js
+++ appsProject/app/hooks/useResults.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import yelp from '../api/yelp';
+import { doGet } from '../api/yelp';
 import constants from '../constants';
 
 export default () => {
@@ -12,13 +12,12 @@ export default () => {
 
   const searchApi = async (searchTerm) => {
     try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose',
-        },
-      });
+      const params = {
+        limit: 50,
+        term: searchTerm,
+        location: 'san jose',
+      };
+      const response = await doGet('/search', params);
       setResult(response.data.businesses);
     } catch (err) {
       setError(constants.GENERAL_ERROR_MESSAGE);
